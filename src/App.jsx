7f2e81873086a1b5e390be1c0554c252a9088110
Sam <118877482+sam-0905@users.noreply.github.com>
2024-09-11 23:03:53 +0530

    updated css and logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,112 +1,63 @@
-import { useState } from "react";
 import "./App.css";
 // import { useState } from 'react'
 import './App.css'
 import "font-awesome/css/font-awesome.min.css";
+import { Routes, Route } from 'react-router-dom';
+import Body from "./Body";
+import LoginComp from "./Pages/Login";
+import CartCompo from "./Pages/Cart";
+import WishListCompo from "./Pages/Wishlist";
+import Error from "./components/Error";
 
-// import faker from "faker";
-// import * as faker from '@faker-js/faker';
-import { faker } from '@faker-js/faker';
+// import { Link } from 'react-router-dom';
+//Error
+// import { NotFound } from './Pages/Error';
+//Nav link
+import { NavLink } from 'react-router-dom';
+// import LoginComponent from './Pages/Login';
+// import UserComponent from './Pages/User';
 
-
-faker.seed(123);
-
-// eslint-disable-next-line no-unused-vars
-const data = [...Array(50)].map((item) => ({
-  id: faker.string.uuid(),
-  name: faker.commerce.productName(),
-  image: faker.image.url({width:128,height:128}),
-  price: faker.commerce.price(),
-  material: faker.commerce.productMaterial(),
-  brand: faker.lorem.word(),
-  inStock: faker.datatype.boolean(),
-  fastDelivery: faker.datatype.boolean(),
-  ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
-  offer: faker.helpers.arrayElement([
-    "Save 50",
-    "70% bonanza",
-    "Republic Day Sale",
-  ]),
-  idealFor: faker.helpers.arrayElement([
-    "Men",
-    "Women",
-    "Girl",
-    "Boy",
-    "Senior",
-  ]),
-  level: faker.helpers.arrayElement([
-    "beginner",
-    "amateur",
-    "intermediate",
-    "advanced",
-    "professional",
-  ]),
-  color: faker.color.human(),
-}));
+const getActiveLink = ({ isActive, isPending }) => ({
+  margin: '1rem 0',
+  color: isActive ? 'yellow' : isPending ? 'pending' : 'cornsilk',
+});
 
 function App() {
-  const [searchText, setSearchText] = useState();
-  const [filteredData, setFilteredData] = useState(data);
-
-  const filterSearchData = () => {
-    console.log("data from filter", searchText);
-    const filteredResult = data.filter((data) =>
-      data.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    console.log({ filteredResult }); 
-    setFilteredData(filteredResult);
-  };
-
-  console.log("data from filter", data);
-
   return (
     <>
-      <h1>Ecommerce App</h1>
+      <nav className="Nav link ">
+        <NavLink style={getActiveLink} to="/">
+        <i className="fa fa-home"></i>
+          Home
+        </NavLink>
+        <NavLink style={getActiveLink} to="/login">
+          <i className="fa fa-sign-in" ></i>
+          Login
+        </NavLink>
+        <NavLink style={getActiveLink} to="/cart">
+        <i className="fa fa-cart-arrow-down "></i>
+          cart
+        </NavLink>
+        <NavLink style={getActiveLink} to="/wish">
+        <i className="fa fa-heart"></i>
+          Wishlist
+        </NavLink>
+      </nav>
 
-      <div className="Search-container">
-        <input
-          type="text"
-          className="search-bar"
-          placeholder="Search for Products, Brands and More"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-        />
-        <button className="search-btn" onClick={filterSearchData}>
-        <i className="fa fa-search"></i>
-        </button>
-      </div>
-      <div className="card" >
-        {filteredData.map(
-          ({
-            id,
-            name,
-            image,
-            price,
-            productName,
-            inStock,
-            level,
-            fastDelivery,
-          }) => (
-            <div key={id} className="card-img">
-              <>
-              <img src={image} width="100%" height="auto" alt={productName} />
-              <h3> {name} </h3>
-              </>
-            <div className="card-body">
-                <div>Rs. {price}</div>
-                    {inStock && <div> In Stock </div>}
-                    {!inStock && <div> Out of Stock </div>}
-                    <div>{level}</div>
-                     {fastDelivery ? (
-                     <div> Fast Delivery </div>
-                     ) : (
-                     <div> 3 days minimum </div>
-                     )}
-             </div>
-          </div>
-          )
-        )}
-      </div>
+      <Routes>
+        <Route path="/" element={<Body/>} />
+        <Route path="/login" element={<LoginComp />} />
+        <Route path="/cart" element={<CartCompo />} />
+        <Route path="/wish" element={<WishListCompo />} />
+        <Route path="*" element={<Error />} /> 
+
+        {/* <Route path="/user" element={<UserComponent />} /> */}
+
+        {/* <Route path="/about" element={<AboutComponent />} /> */}
+        {/* this route shld also map to a component. */}
+        {/* <Route path="/product/:ProductId" element={<ProductComponent />} />
+        <Route path="/cateogry" element={<Category />} />*/}
+      </Routes>
     </>
   );
 }
---
+++ src/Body.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+// import faker from "faker";
+// import * as faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
+
+
+faker.seed(123);
+
+// eslint-disable-next-line no-unused-vars
+const data = [...Array(50)].map((item) => ({
+  id: faker.string.uuid(),
+  name: faker.commerce.productName(),
+  image: faker.image.url({width:128,height:128}),
+  price: faker.commerce.price(),
+  material: faker.commerce.productMaterial(),
+  brand: faker.lorem.word(),
+  inStock: faker.datatype.boolean(),
+  fastDelivery: faker.datatype.boolean(),
+  ratings: faker.helpers.arrayElement([1, 2, 3, 4, 5]),
+  offer: faker.helpers.arrayElement([
+    "Save 50",
+    "70% bonanza",
+    "Republic Day Sale",
+  ]),
+  idealFor: faker.helpers.arrayElement([
+    "Men",
+    "Women",
+    "Girl",
+    "Boy",
+    "Senior",
+  ]),
+  level: faker.helpers.arrayElement([
+    "beginner",
+    "amateur",
+    "intermediate",
+    "advanced",
+    "professional",
+  ]),
+  color: faker.color.human(),
+}));
+
+function Body() {
+  const [searchText, setSearchText] = useState();
+  const [filteredData, setFilteredData] = useState(data);
+
+  const filterSearchData = () => {
+    console.log("data from filter", searchText);
+    const filteredResult = data.filter((data) =>
+      data.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+    console.log({ filteredResult }); 
+    setFilteredData(filteredResult);
+  };
+
+  console.log("data from filter", data);
+
+  return (
+    <>
+      <h1>Ecommerce App</h1>
+
+      <div className="Search-container">
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search for Products, Brands and More"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button className="search-btn" onClick={filterSearchData}>
+        <i className="fa fa-search"></i>
+        </button>
+      </div>
+      <div className="card" >
+        {filteredData.map(
+          ({
+            id,
+            name,
+            image,
+            price,
+            productName,
+            inStock,
+            level,
+            fastDelivery,
+          }) => (
+            <div key={id} className="card-img">
+              <>
+              <img src={image} width="100%" height="auto" alt={productName} />
+              <h3> {name} </h3>
+              </>
+            <div className="card-body">
+                <div>Rs. {price}</div>
+                    {inStock && <div> In Stock </div>}
+                    {!inStock && <div> Out of Stock </div>}
+                    <div>{level}</div>
+                     {fastDelivery ? (
+                     <div> Fast Delivery </div>
+                     ) : (
+                     <div> 3 days minimum </div>
+                     )}
+             </div>
+          </div>
+          )
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Body;
