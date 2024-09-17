0a16bbea6ea0ae33e03aab1c6ed82f0b65a6799d
Sam <118877482+sam-0905@users.noreply.github.com>
2024-09-17 08:41:11 +0530

    Removed Price folder and imports from Sidebar component

--- src/Sidebar/price/Price.jsx
+++
@@ -1,6 +0,0 @@
-const Price = () =>{
-    return(    <h1>this is cart</h1>
-    )
-}
-
-export default Price
--- src/Sidebar/sidebar.jsx
+++ src/Sidebar/sidebar.jsx
@@ -1,12 +1,10 @@
 import "./sidebar.css"
 import Category from "./Category/Category";
-import Price from "./price/price";
 const SideBar = () => {
 return (
     <>
     <section className="sidebar-main">
     <Category/> 
-    <Price/>
     </section>
     
     </>
